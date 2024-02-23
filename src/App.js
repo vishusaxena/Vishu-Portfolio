e1be2b1209b9c396a1265f85c4ac219da7160f4b
vishusaxena <vishusaxena3105@gmail.com>
2024-02-23 21:14:20 +0530

    Added projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import About from "./components/About";
 import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
 
 
 function App() {
   return (
     <>
     <NavBar/>
-    <About/></>
+    <Projects/>
+    </>
   );
 }
 
