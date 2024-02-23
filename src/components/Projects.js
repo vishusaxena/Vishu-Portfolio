11acf6099f5d10f18936c8d840fb8dd1f6399a28
vishusaxena <vishusaxena3105@gmail.com>
2024-02-23 21:18:37 +0530

    refactored

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,48 +8,48 @@ export default function Projects() {
     return (
         <>
         <div className='container'>
-        <div class=" row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card shadow p-3 mb-5 border-primary">
-      <img src={logo} class="card-img-top" alt=""/>
-      <div class="card-body">
-        <h5 class="card-title">Newsify</h5>
-        <p class="card-text">"Newsify is a dynamic React application designed to keep users informed with the latest news headlines. Leveraging API integration, Newsify fetches real-time news data from trusted sources, presenting it in a user-friendly interface.</p>
-        <a href="https://github.com/vishusaxena/Newsify.git"  target='_blank' class="btn btn-primary">Go to Github</a>
+        <div className=" row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card shadow p-3 mb-5 border-primary">
+      <img src={logo} className="card-img-top" alt="Newsify"/>
+      <div className="card-body">
+        <h5 className="card-title">Newsify</h5>
+        <p className="card-text">"Newsify is a dynamic React application designed to keep users informed with the latest news headlines. Leveraging API integration, Newsify fetches real-time news data from trusted sources, presenting it in a user-friendly interface.</p>
+        <a href="https://github.com/vishusaxena/Newsify.git"  target='_blank' className="btn btn-primary">Go to Github</a>
 
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card shadow p-3 mb-5 border-primary">
-      <img src={logo} class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">TextonTrack</h5>
-        <p class="card-text"> "TextonTrack" is a versatile React project designed to streamline text manipulation tasks with ease. Whether you're looking to convert text to uppercase or lowercase, count characters or words, this app has you covered. 
+  <div className="col">
+    <div className="card shadow p-3 mb-5 border-primary">
+      <img src={logo} className="card-img-top" alt="TextonTrack" />
+      <div className="card-body">
+        <h5 className="card-title">TextonTrack</h5>
+        <p className="card-text"> "TextonTrack" is a versatile React project designed to streamline text manipulation tasks with ease. Whether you're looking to convert text to uppercase or lowercase, count characters or words, this app has you covered. 
 .</p>
-<a href="https://github.com/vishusaxena/TextonTrack.git"  target='_blank' class="btn btn-primary">Go to Github</a>
+<a href="https://github.com/vishusaxena/TextonTrack.git"  target='_blank' className="btn btn-primary">Go to Github</a>
 
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card shadow p-3 mb-5 border-primary">
-      <img src={crud} class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">TextFileManager</h5>
-        <p class="card-text">"TextFileManager" is a C++ project designed to facilitate CRUD (Create, Read, Update, Delete) operations on text files. With TextFileManager, users can seamlessly manage text files, including tasks such as adding, viewing, updating, and deleting text entries within the files. </p>
-        <a href="https://github.com/vishusaxena/TextFIleManager.git"  target='_blank' class="btn btn-primary">Go to Github</a>
+  <div className="col">
+    <div className="card shadow p-3 mb-5 border-primary">
+      <img src={crud} className="card-img-top" alt="CRUD" />
+      <div className="card-body">
+        <h5 className="card-title">TextFileManager</h5>
+        <p className="card-text">"TextFileManager" is a C++ project designed to facilitate CRUD (Create, Read, Update, Delete) operations on text files. With TextFileManager, users can seamlessly manage text files, including tasks such as adding, viewing, updating, and deleting text entries within the files. </p>
+        <a href="https://github.com/vishusaxena/TextFIleManager.git"  target='_blank' className="btn btn-primary">Go to Github</a>
 
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card shadow p-3 mb-5 border-primary">
-      <img src={quiz} class="card-img-top" alt="pro"/>
-      <div class="card-body">
-        <h5 class="card-title">QuizLab</h5>
-        <p class="card-text">"QuizLab" is your go-to destination for interactive quizzes on a wide range of technical and non-technical subjects. Developed with HTML, CSS, and JavaScript, QuizLab offers a dynamic and engaging platform for users to test their knowledge for the better future ahead.</p>
-        <a href="https://github.com/vishusaxena/QuizLab.git"  target='_blank' class="btn btn-primary">Go to Github</a>
+  <div className="col">
+    <div className="card shadow p-3 mb-5 border-primary">
+      <img src={quiz} className="card-img-top" alt="QuizLab"/>
+      <div className="card-body">
+        <h5 className="card-title">QuizLab</h5>
+        <p className="card-text">"QuizLab" is your go-to destination for interactive quizzes on a wide range of technical and non-technical subjects. Developed with HTML, CSS, and JavaScript, QuizLab offers a dynamic and engaging platform for users to test their knowledge for the better future ahead.</p>
+        <a href="https://github.com/vishusaxena/QuizLab.git"  target='_blank' className="btn btn-primary">Go to Github</a>
 
       </div>
     </div>
