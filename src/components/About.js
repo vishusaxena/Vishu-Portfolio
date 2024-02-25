7e61212b14edd78853588ce889bc625cfb3e38d3
vishusaxena <vishusaxena3105@gmail.com>
2024-02-25 21:38:15 +0530

    refactored About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,10 @@ export default function About(){
 
     return(
         <>
+        <div className=' d-flex sm:d-block'>
+        
         <div className='w-50 custom-container ' style={{marginTop:"150px"}}>
+        
            <h1>Hi &#x1F590;, I am </h1>
            <h1>Vishu Saxena</h1>
            <h4>
@@ -14,12 +17,17 @@ I am  a <b>Web Developer and Competitive coding enthusiast</b>
         <p>
         I am an undergrad and pursuing B.Tech in Computer Science and Engineering from SRMS Institute.I have good knowledge about C++,Python, Problem solving and Web Development fundamentals as I has build projects related to it.
         </p>
+        
+        
         <a href="# " download className ="btn btn-outline-primary btn-lg " role="button" aria-pressed="true">Download CV</a>
         <button type="button" className="btn btn-lg btn-outline-primary mx-3">Let's Connect!</button>
+       
+        </div>
+        <div className='container w-50 ' style={{marginTop:"170px ",marginLeft:"120px"}}>
+        <img  alt="Coding" width="400" src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif"/>
 
         </div>
-        
-        
+        </div>
         
         </>    );
 
