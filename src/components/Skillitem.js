bbfeab9cd0faf1e4354b09d62753fdfeeeb3fd17
vishusaxena <vishusaxena3105@gmail.com>
2024-02-26 21:39:29 +0530

    added some info about my skills

--- src/components/Skillitem.js
+++ src/components/Skillitem.js
@@ -8,7 +8,7 @@ export default function Skillitem(props) {
                             <a href={props.url} target="_blank" rel="noreferrer"> <img className='m-2 'src={props.imageUrl} alt="c" width="60" height="60" /> </a>
                             <div className="card-body">
                                 <h5 className="card-title">{props.skill}</h5>
-                                <p className="card-text">This is a longer card border border-whitewith supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text">{props.dis}</p>
                             </div>
                         </div>
                     </div>
