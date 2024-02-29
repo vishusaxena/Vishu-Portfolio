d5d4fd8f185c1015968f6fa635ce01b011d1e053
vishusaxena <vishusaxena3105@gmail.com>
2024-02-29 18:00:04 +0530

    Altered Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import logo from'./Newsify.jpg'
-import crud from './crud.png'
-import quiz from './quiz.jpg'
+
 
 
 export default function Projects() {
@@ -12,7 +10,6 @@ export default function Projects() {
         <div className=" row row-cols-1 row-cols-md-2 g-4 my-3">
   <div className="col">
     <div className="card  shadow p-3 mb-5 border-primary">
-      <img src={logo} className="card-img-top" alt="Newsify"/>
       <div className="card-body">
         <h5 className="card-title">Newsify</h5>
         <p className="card-text">"Newsify is a dynamic React application designed to keep users informed with the latest news headlines. Leveraging API integration, Newsify fetches real-time news data from trusted sources, presenting it in a user-friendly interface.</p>
@@ -23,7 +20,6 @@ export default function Projects() {
   </div>
   <div className="col">
     <div className="card shadow p-3 mb-5 border-primary">
-      <img src={logo} className="card-img-top" alt="TextonTrack" />
       <div className="card-body">
         <h5 className="card-title">TextonTrack</h5>
         <p className="card-text"> "TextonTrack" is a versatile React project designed to streamline text manipulation tasks with ease. Whether you're looking to convert text to uppercase or lowercase, count characters or words, this app has you covered. 
@@ -35,7 +31,6 @@ export default function Projects() {
   </div>
   <div className="col">
     <div className="card shadow p-3 mb-5 border-primary">
-      <img src={crud} className="card-img-top" alt="CRUD" />
       <div className="card-body">
         <h5 className="card-title">TextFileManager</h5>
         <p className="card-text">"TextFileManager" is a C++ project designed to facilitate CRUD (Create, Read, Update, Delete) operations on text files. With TextFileManager, users can seamlessly manage text files, including tasks such as adding, viewing, updating, and deleting text entries within the files. </p>
@@ -46,7 +41,6 @@ export default function Projects() {
   </div>
   <div className="col">
     <div className="card shadow p-3 mb-5 border-primary">
-      <img src={quiz} className="card-img-top" alt="QuizLab"/>
       <div className="card-body">
         <h5 className="card-title">QuizLab</h5>
         <p className="card-text">"QuizLab" is your go-to destination for interactive quizzes on a wide range of technical and non-technical subjects. Developed with HTML, CSS, and JavaScript, QuizLab offers a dynamic and engaging platform for users to test their knowledge for the better future ahead.</p>
